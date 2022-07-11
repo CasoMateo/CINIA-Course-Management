44fd9f9247d9e9e55b4670a181bfe1d35787fa19
Mateo Caso <casomateo8@gmail.com>
2022-07-11 11:46:32 -0500

    addition of separate course route

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes, useNavigate, Navigate } from 'r
 import Users from '../admin_components/Users';
 import User from '../admin_components/User';
 import Courses from '../admin_components/Courses';
+import Course from '../admin_components/Course';
 import CourseFeed from '../operator_components/CourseFeed';
 import ExploreCourse from '../operator_components/ExploreCourse';
 
@@ -19,7 +20,7 @@ function AdminRoutes(props) {
         <Route path = '/usuarios' element = { <Users /> } /> 
         <Route path = '/cursos' element = { <Courses /> }  /> 
         <Route path = '/usuario/:cur_user' element = { <User /> }  /> 
-
+        <Route path = '/curso/:cur_course' element = { <Course /> }  /> 
         <Route path = '/operador' element = { <CourseFeed /> } />
         <Route path = '/operador/:cur_operator_course' element = { <ExploreCourse /> } />
         <Route path = '*' element = { <Navigate to = '/usuarios' /> }/>
