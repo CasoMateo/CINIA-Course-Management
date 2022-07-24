ef2e55817ac36b33e8b65f1f180355526be8b068
CasoMateo <casomateo8@gmail.com>
2022-07-23 21:37:57 -0500

    adjustment of route access based on permission

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import React, { useState, useContext } from 'react'; 
 import AdminRoutes from './routes/AdminRoutes'; 
 import OperatorRoutes from './routes/OperatorRoutes'; 
+import UnprotectedRoutes from './routes/UnprotectedRoutes';
+import { AuthContext } from './contexts/AuthContext'; 
 
 function App() {
 
-  const admin = true; 
-  const status = true; 
+  const { status, admin } = useContext(AuthContext);
 
   return (
-  
-
-    admin ? 
+    
+    !status ?
+    <UnprotectedRoutes /> :
+    (admin ? 
     <AdminRoutes /> : 
-    <OperatorRoutes />
+    <OperatorRoutes />)
       
-
     
   );
 }
